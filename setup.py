32a3175c3429d627bcf91238dce3175f3c289eb3
ric70x7 <ric70x7@gmail.com>
2019-01-22 06:00:36 -0800

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='buffers',
+      version='0.1',
+      description='Some tools to define buffers',
+      url='http://github.com/storborg/funniest',
+      author='Ricardo Andrade',
+      author_email='',
+      license='MIT',
+      packages=['buffers'],
+      zip_safe=False)
