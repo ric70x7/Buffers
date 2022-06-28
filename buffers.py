c4dea04a34b0ea209e64cfcfa34ad95043e82dc8
Ricardo <ric70x7@gmail.com>
2022-06-28 01:21:21 -0500

    updated

--- buffers.py
+++ buffers.py
@@ -1,88 +1,122 @@
 import numpy as np
-import geopandas as geop
+import geopandas as gpd
 from shapely import geometry
 from shapely.ops import polygonize
-from scipy.spatial import Voronoi
-import json
+from scipy.spatial import Voronoi, ConvexHull
 
-def voronoi_polygons(X, margin=0):
+
+def clouds(X: np.ndarray, radius: list, num_points: float=8):
+    vertex = np.pi * np.linspace(0, 2, num_points + 1)
+
+    if len(radius) != 2:
+        raise ValueError('radius length must be 2.')
+
+    X_clouds = [
+        np.vstack(
+            [
+                xi + np.array(
+                    [
+                        radius[0] * np.cos(t), radius[1] * np.sin(t)
+                    ]
+                ) for t in vertex
+            ]
+        ) for xi in X
+    ]
+    return X_clouds
+
+
+def buffer(X: np.ndarray, radius: list=None, num_points: int=12):
+
+    if radius is not None:
+        X_cloud = np.vstack(clouds(X, radius=radius, num_points=num_points))
+        X = np.vstack([X, X_cloud])
+    convex_hull = ConvexHull(X)
+    points = convex_hull.points[convex_hull.vertices]
+    polygon = geometry.Polygon(points)
+    return polygon
+
+
+def regular_polygons(X: np.ndarray, radius: list, num_angles: int=8):
+    '''
+    Return a set of regular polygons around points X.
+
+    :param X: Array of points with shape = [n, 2].
+
+    :param radius: Circumradius of the polygon.
+
+    :param num_angles: Number of angles of each polygon.>= 3.
+
+    :return: Geopandas data frame.
+    '''
+
+    polygons = clouds(X, radius=radius, num_points=num_angles)
+    return gpd.GeoDataFrame(
+        {
+            'geometry':
+            geometry.MultiPolygon([geometry.Polygon(pi) for pi in polygons])
+        }
+    )
+
+
+def voronoi_polygons(X: np.ndarray, radius: list = None):
     '''
     Returns a set of Voronoi polygons corresponding to a set of points X.
 
     :param X: Array of points (optional).
               Numpy array, shape = [n, 2].
 
-    :param margin: Minimum margin to extend the outer polygons of the tessellation.
-                   Non-negative float.
+    :param radius: Minimum margin to extend the outer polygons of the tessellation.
 
     :return: Geopandas data frame.
     '''
-    assert isinstance(X, np.ndarray), 'Expecting a numpy array.'
-    assert X.ndim == 2, 'Expecting a two-dimensional array.'
-    assert X.shape[1] == 2, 'Number of columns is different from expected.'
-    n_points = X.shape[0]
+    if X.shape[1] != 2:
+        raise ValueError('Function only implemented for 2D spaces.')
+
+    if radius is None:
+        margin = [0, 0]
+    elif len(radius) != 2:
+        raise ValueError('radius length must be 2.')
+    else:
+        margin = radius
+
+    num_points = X.shape[0]
 
     c1, c2 = np.sort(X[:, 0]), np.sort(X[:, 1])
-    _diffs = np.array([max(margin, np.diff(c1).mean()), max(margin, np.diff(c2).mean())])
+    _diffs = np.array(
+        [
+            max(margin[0], np.diff(c1).mean()),
+            max(margin[1], np.diff(c2).mean())
+        ]
+    )
 
     min_c1, min_c2 = X.min(0) - _diffs
     max_c1, max_c2 = X.max(0) + _diffs
 
-    extra_points = np.vstack([np.vstack([np.repeat(min_c1, n_points), c2]).T,
-                              np.vstack([np.repeat(max_c1, n_points), c2]).T,
-                              np.vstack([c1, np.repeat(min_c2, n_points)]).T,
-                              np.vstack([c1, np.repeat(max_c2, n_points)]).T])
+    box = np.vstack([
+        np.vstack([np.repeat(min_c1, num_points), c2]).T,
+        np.vstack([np.repeat(max_c1, num_points), c2]).T,
+        np.vstack([c1, np.repeat(min_c2, num_points)]).T,
+        np.vstack([c1, np.repeat(max_c2, num_points)]).T
+    ])
 
-    _X = np.vstack([X, extra_points])
+    _X = np.vstack([X, box])
 
     # Define polygons geometry based on tessellation
     vor = Voronoi(_X)
     lines = [geometry.LineString(vor.vertices[li]) for li in vor.ridge_vertices if -1 not in li]
     disord = geometry.MultiPolygon(list(polygonize(lines)))
-    ix_order = np.array([[i for i, di in enumerate(disord) if di.contains(geometry.Point(pi))]
+    ix_order = np.array([[i for i, di in enumerate(disord.geoms) if di.contains(geometry.Point(pi))]
                          for pi in X]).ravel()
 
-    return geop.GeoDataFrame({'geometry': geometry.MultiPolygon([disord[i] for i in ix_order])})
+    polygons = gpd.GeoDataFrame({'geometry': geometry.MultiPolygon([disord.geoms[i] for i in ix_order])})
+
+    mask = buffer(X, radius=radius)
+    polygons = polygons.clip(mask)
+
+    return polygons.sort_index()
 
 
-def regular_polygons(X, radius, n_angles=8):
-    '''
-    Return a set of regular polygons around points X.
-
-    :param X: Array of points (optional).
-              Numpy array, shape = [n, 2].
-
-    :param radius: Circumradius of the polygon.
-                   Positive float.
-
-    :param n_angles: Number of angles of each polygon.
-                     Integer >= 3.
-
-    :return: Geopandas data frame.
-    '''
-    assert isinstance(X, np.ndarray), 'Expecting a numpy array.'
-    assert X.ndim == 2, 'Expecting a two-dimensional array.'
-    assert X.shape[1] == 2, 'Number of columns is different from expected.'
-
-    assert isinstance(n_angles, int), 'n_angles must be an integer.'
-    assert n_angles >= 3, 'Angles must be greater than two.'
-
-    vertex = np.pi * np.linspace(0, 2, n_angles + 1)
-
-    if isinstance(radius, float):
-        assert radius > 0, 'Radius must be positive.'
-        polys = [np.vstack([xi + radius * np.array([np.cos(t), np.sin(t)]) for t in vertex]) for xi in X]
-    else:
-        assert isinstance(radius, np.ndarray), 'Expecting a numpy array.'
-        assert radius.ndim == 1, 'Expecting a one-dimensional array.'
-        assert radius.size == X.shape[0], 'Array size is different from expected.'
-
-        polys = [np.vstack([xi + ri * np.array([np.cos(t), np.sin(t)]) for t in vertex]) for xi, ri in zip(X, radius)]
-
-    return geop.GeoDataFrame({'geometry': geometry.MultiPolygon([geometry.Polygon(pi) for pi in polys])})
-
-
-def disjoint_polygons(X, radius, n_angles=8):
+def disjoint_polygons(X: np.ndarray, radius: list, num_angles: int=8):
     '''
     Return a set of disjoint polygons around points X.
 
@@ -92,25 +126,14 @@ def disjoint_polygons(X, radius, n_angles=8):
     :param radius: Circumradius of the polygon.
                    Positive float.
 
-    :param n_angles: Number of angles of each polygon.
+    :param num_angles: Number of angles of each polygon.
                      Integer >= 3.
 
     :return: Geopandas data frame.
     '''
-    vorpol = voronoi_polygons(X, margin=2*np.max(radius))
-    regpol = regular_polygons(X, radius=radius, n_angles=n_angles)
-    dispol = [vi.intersection(pi) for vi,pi in zip(vorpol.geometry, regpol.geometry)]
-
-    return geop.GeoDataFrame({'geometry': geometry.MultiPolygon(dispol)})
-
-
-def dock_deploy(js):
-    X = json.loads(js)
-    X = np.array(X)
-    P = voronoi_polygons(X)
-    output = json.dumps(True)
-    return output
-
-
-
-
+    vorpol = voronoi_polygons(X, radius=radius)
+    regpol = regular_polygons(X, radius=radius, num_angles=num_angles)
+    dispol = [
+        vi.intersection(pi) for vi,pi in zip(vorpol.geometry, regpol.geometry)
+    ]
+    return gpd.GeoDataFrame({'geometry': geometry.MultiPolygon(dispol)})
